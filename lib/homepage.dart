e7f388750084abded2257cc4c38c4226c937e00b
Yavuzsavan <ysavan271@gmail.com>
2023-01-03 17:04:16 +0300

    navigation bar changed icons added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -13,8 +13,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int selectedIndex = 0;
+  int selectedIndex = 3;
 
+  void navigateBottomBar(int index){
+    setState(() {
+      selectedIndex = index;
+    });
+  }
   final List<Widget> pages = [
     const UserStep(),
     const UserWeight(),
@@ -25,43 +30,66 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[selectedIndex],
-      bottomNavigationBar: Container(
-        color: Colors.black,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
-          child: GNav(
-            backgroundColor: Colors.black87,
-            color: Colors.white,
-            activeColor: Colors.white,
-            gap: 4,
-            onTabChange: (index) {
-              setState(() {
-                selectedIndex = index;
-              });
-            },
-            tabBackgroundColor: Colors.grey.shade800,
-            padding: const EdgeInsets.all(4),
-            tabs: const [
-              GButton(
-                icon: Icons.home,
-                text: "Steps",
-              ),
-              GButton(
-                icon: Icons.calendar_month_outlined,
-                text: "Weight",
-              ),
-              GButton(
-                icon: Icons.ac_unit_outlined,
-                text: "Training",
-              ),
-              GButton(
-                icon: Icons.person,
-                text: "Me",
-              ),
-            ],
-          ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: navigateBottomBar,
+        currentIndex: selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+        BottomNavigationBarItem(
+          icon: Image.asset("assets/icons/steps.png",width: 30, height: 30,),
+          label: "Steps",
         ),
-      ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_month_outlined),
+          label: "Calendar",
+        ),
+        BottomNavigationBarItem(
+          icon: Image.asset("assets/icons/weight.png",width: 30, height: 30,),
+          label: "Trainning",
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.person_outlined),
+          label: "Profile",
+        ),
+      ]),
+
+      // bottomNavigationBar: Container(
+      //   color: Colors.black,
+      //   child: Padding(
+      //     padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+      //     child: GNav(
+      //       backgroundColor: Colors.black87,
+      //       color: Colors.white,
+      //       activeColor: Colors.white,
+      //       gap: 4,
+      //       onTabChange: (index) {
+      //         setState(() {
+      //           selectedIndex = index;
+      //         });
+      //       },
+      //       tabBackgroundColor: Colors.grey.shade800,
+      //       padding: const EdgeInsets.all(4),
+      //       tabs: const [
+      //         GButton(
+      //           icon: Icons.home,
+      //           text: "Steps",
+      //         ),
+      //         GButton(
+      //           icon: Icons.calendar_month_outlined,
+      //           text: "Weight",
+      //         ),
+      //         GButton(
+      //           icon: Icons.ac_unit_outlined,
+      //           text: "Training",
+      //         ),
+      //         GButton(
+      //           icon: Icons.person,
+      //           text: "Me",
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
